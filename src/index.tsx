4092ad4c51314d1adbee8a96988e1735bc9b66b0
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-28 16:19:46 -0400

    type interface with state

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 // 1) Import ReactDOM library
 import ReactDOM from "react-dom/client";
 import { Parent } from "./props/Parent";
+import GuestList from "./state/GuestList";
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById("root");
@@ -12,8 +13,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
   return (
     <div>
-      <h1>Hi there!</h1>
-      <Parent />
+      <GuestList />
     </div>
   );
 };
--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,11 +2,19 @@ import React, { useState } from "react";
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState("");
+  //need to define what is to be stored in arrays
+  const [guests, setGuests] = useState<string[]>([]);
+
+  const onClick = () => {
+    setName("");
+    setGuests([...guests, name]);
+  };
+
   return (
     <div>
       <h1>GuestList</h1>
       <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button>Add guest</button>
+      <button onClick={onClick}>Add guest</button>
     </div>
   );
 };
