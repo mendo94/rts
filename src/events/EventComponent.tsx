e8d5b6932e0e99e015167131bd20c84fb3f79861
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-02 20:33:26 -0400

    command click on event for index of events

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -2,9 +2,16 @@ const EventComponent: React.FC = () => {
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event);
   };
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event.bubbles);
+  };
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
     </div>
   );
 };
