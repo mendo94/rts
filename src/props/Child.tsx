f372c49cf1c1bcf330aa18fa34b3164b203546b0
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-07 21:33:14 -0400

    .

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -16,7 +16,7 @@ export const Child = ({ color, onClick }: ChildProps) => {
   );
 };
 
-// react now knows this is a react function component and can pass more types such as context or props..
+// react now knows this is a react function component and can pass more types such as context or props...
 export const Example: React.FC<ChildProps> = ({ color, onClick, children }) => {
   return (
     <div>
